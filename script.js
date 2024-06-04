58d879fe58b165b087255adb6a6470592b3881c0
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-06-04 17:23:06 -0300

    responsividade

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 $(document).ready(function() {
-    const sections = $('section');
-    const navItem = $('.flex-container')
-
-    $(window).on('scroll', function(){
-        const header = $('header')
-        const scrollPosition = $(window).scrollTop() - header.outerHeight()
-        if(scrollPosition <= 0) {
-            header.css('box-shadow', 'none')
-        } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)')
-        }
-    })
-    ScrollReveal().reveal('.services', {
-        origin:'left',
-        duration:2000,
-        distance:'20%'
-    })
+   $('#mobile_btn').on('click', function() {
+    $('#mobile_menu').toggleClass('active')
+    $('#mobile_btn').find('i').toggleClass('fa-x') // find para achar o i que fará a troca de classe
+   })
     
 })
